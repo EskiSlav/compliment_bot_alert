532bdc4fdb6c4cbc0449c133ea73bc53fca10c71
Viacheslav Kozachok <viak@opinov8.com>
2023-03-10 03:14:02 +0200

    OpenAI works

--- bot/func/func.py
+++ bot/func/func.py
@@ -8,10 +8,11 @@ from aws.models import NewCompliment
 from aws.ssm import get_ssm_parameter_value
 from tg.bot import Bot
 from tg.models import User
-logger = logging.getLogger(__name__)
 
-bot = Bot(get_ssm_parameter_value('/config/bot/api_token'))
+
+logger = logging.getLogger(__name__)
 db = DynamoDB()
+bot = Bot(get_ssm_parameter_value('/config/bot/api_token'))
 
 
 def generate_compliment():
@@ -22,18 +23,18 @@ def generate_compliment():
         max_tokens=2048,
     )
     logger.debug(f'OpenAI {response=}')
+    print(f'OpenAI {response=}')
     return response['choices'][0]['text']
 
 
 def send_compliments():
     logger.debug('Starting sending compliments')
-    openai_api = get_ssm_parameter_value('/config/bot/openai_api_token')
-    openai.api_key = openai_api
 
     users = db.get_all_users()
 
     for user in users:
         u = User(**user)
+        print(u)
         compliment = NewCompliment(generate_compliment())
         db.put_compliment(compliment)
         bot.send_message(u.id, compliment.compliment_ua)
